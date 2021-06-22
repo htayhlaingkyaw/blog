a275019b964619d4e3930c18dedb3c2f704fbd11
htayhlaingkyaw <htayhlaingkyaw92@gmail.com>
2021-06-22 15:30:05 +0900

    init

---
+++ src/main/java/com/example/blog/repository/TutorialRepository.java
@@ -0,0 +1,16 @@
+package com.example.blog.repository;
+
+import com.example.blog.model.Tutorial;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long>{
+    
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+
+}
